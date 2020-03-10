2dbbca29e2efac503566a7f989bc617fcd6f34cf
mmomchilov <mmomchilov@csc.com>
2020-03-10 23:24:37 +0200

    Add bootstrap css

--- src/app/theme/components/baCollectionDetails/baCollectionDetails.component.ts
+++ src/app/theme/components/baCollectionDetails/baCollectionDetails.component.ts
@@ -22,6 +22,7 @@ export class BaCollectionDetails implements OnInit, DoCheck {
   get formGroup() {
     return this.formGroupValue;
   }
+  @Input() recipientData: any;
 
   formGroupValue: FormGroup;
 
